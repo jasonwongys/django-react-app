68857ced894143380dbcb962e73e6e7a5da79337
jasonwongys <jasonwongys@hotmail.com>
2024-08-26 00:32:05 +0800

    add apiurl

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "/choreo-apis/djangoreactapp/backend/v1";
 
 
 const api = axios.create({
